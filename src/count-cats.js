222b25df95164c8d43fd5eb7202ace04b3ba3b94
RVeronica <akinrv16@gmail.com>
2021-03-29 20:49:15 +0300

    implement carbon-dating and count-cats funcs

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let res=0;
+  for(let subArr of matrix){
+    for(let elem of subArr){
+    if(String(elem)==='^^') res++;
+    }
+  }
+  if(res===0) return 0;
+  else return res;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!members || members.constructor!==Array) return false;
+  let newArr = '';
+  for(let substr in members){
+    if(typeof(substr)==='string')
+      substr = substr.trim();
+    newArr =''+ substr[0];
+  }
+  newArr = newArr.toUpperCase().split('').sort().join('');
+  return newArr;
 };
